fdfab74d5ca8675d5e8150de71a6d6f6e46bc0e2
Elia T <eliat158@gmail.com>
2021-04-25 03:25:45 +0300

    bottom snapping sheet for user ID on screen

--- lib/screens/all_suggestions_screen.dart
+++ lib/screens/all_suggestions_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:english_words/english_words.dart';
+import 'package:snapping_sheet/snapping_sheet.dart';
 import 'package:hello_me/service_repos/saved_suggestions_repository.dart';
 import 'package:hello_me/screens/login_screen.dart';
 import 'package:hello_me/screens/saved_suggestions_screen.dart';
@@ -35,11 +39,87 @@ class _AllSuggestionsScreenState extends State<AllSuggestionsScreen> {
             future: savedRepo.isSaved(suggestion),
             builder: (context, AsyncSnapshot<bool> snapshot) {
               final alreadySaved = snapshot.data ?? false;
-              return Icon(
-                  alreadySaved ? Icons.favorite : Icons.favorite_border,
+              return Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
                   color: alreadySaved ? Colors.red : null);
             }),
         onTap: () => savedRepo.toggleSelection(suggestion));
+  }
+
+  Widget _bottomSheetBuilder(BuildContext context, AuthRepository auth,
+      {required Widget child}) {
+    //todo: on tap, change position of sheet to next position
+    if (auth.user == null){
+      return child;
+    }
+
+    final userName = auth.user?.email ?? "WHO ARE YOUUU";
+    return SnappingSheet(
+      child: child,
+      grabbing: Container(
+        color: Colors.blueGrey[100],
+        child: ListTile(
+          title: Text('Welcome back, $userName',
+              style: TextStyle(fontSize: 14)),
+          trailing: Icon(Icons.expand_less),
+        ),
+      ),
+      grabbingHeight: 50,
+      sheetBelow: SnappingSheetContent(
+          child: ColoredBox(
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  child: Text(userName[0]),
+                  radius: 40,
+                  backgroundColor: Colors.blueGrey[200],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(userName, style: TextStyle(fontSize: 18)),
+                    ),
+                    ElevatedButton(
+                      child: Text(
+                        "Change avatar",
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.teal,
+                      ),
+                      onPressed: () => log("tried to change avatar"),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      )),
+      snappingPositions: [
+        SnappingPosition.factor(
+          positionFactor: 0.0,
+          snappingDuration: Duration(milliseconds: 500),
+          grabbingContentOffset: GrabbingContentOffset.top,
+        ),
+        SnappingPosition.factor(
+          positionFactor: 0.25,
+          snappingDuration: Duration(milliseconds: 500),
+        ),
+        SnappingPosition.factor(
+          positionFactor: 0.9,
+          snappingDuration: Duration(milliseconds: 500),
+          grabbingContentOffset: GrabbingContentOffset.bottom,
+        ),
+      ],
+    );
   }
 
   Widget _build(BuildContext context, AuthRepository auth,
@@ -59,14 +139,16 @@ class _AllSuggestionsScreenState extends State<AllSuggestionsScreen> {
                 builder: (context) => LoginScreen().build(context)))));
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Startup Name Generator'),
-          actions: actions,
-        ),
-        body: ListView.builder(
-          padding: const EdgeInsets.all(16),
-          itemBuilder: (context, row) => _rowBuilder(context, row, savedRepo),
-        ));
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: actions,
+      ),
+      body: _bottomSheetBuilder(context, auth,
+          child: ListView.builder(
+            padding: const EdgeInsets.all(16),
+            itemBuilder: (context, row) => _rowBuilder(context, row, savedRepo),
+          )),
+    );
   }
 
   @override
